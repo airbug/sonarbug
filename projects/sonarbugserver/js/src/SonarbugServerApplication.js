42167b15333cf56a4f8f03829182027a403dc614
Brian Neisler <neisler.brian@gmail.com>
2013-10-19 18:48:08 -0700

    Update sonarbug to use new ModuleScan

--- projects/sonarbugserver/js/src/SonarbugServerApplication.js
+++ projects/sonarbugserver/js/src/SonarbugServerApplication.js
@@ -9,8 +9,11 @@
 
 //@Require('Class')
 //@Require('Obj')
-//@Require('bugioc.IocContext')
+//@Require('bugioc.ConfigurationAnnotationProcessor')
 //@Require('bugioc.ConfigurationScan')
+//@Require('bugioc.ModuleAnnotationProcessor')
+//@Require('bugioc.ModuleScan')
+//@Require('bugioc.IocContext')
 
 
 //-------------------------------------------------------------------------------
@@ -24,10 +27,13 @@ var bugpack = require('bugpack').context();
 // BugPack
 //-------------------------------------------------------------------------------
 
-var Class               = bugpack.require('Class');
-var Obj                 = bugpack.require('Obj');
-var IocContext          = bugpack.require('bugioc.IocContext');
-var ConfigurationScan   = bugpack.require('bugioc.ConfigurationScan');
+var Class                               = bugpack.require('Class');
+var Obj                                 = bugpack.require('Obj');
+var IocContext                          = bugpack.require('bugioc.IocContext');
+var ConfigurationAnnotationProcessor    = bugpack.require('bugioc.ConfigurationAnnotationProcessor');
+var ConfigurationScan                   = bugpack.require('bugioc.ConfigurationScan');
+var ModuleAnnotationProcessor           = bugpack.require('bugioc.ModuleAnnotationProcessor');
+var ModuleScan                          = bugpack.require('bugioc.ModuleScan');
 
 
 //-------------------------------------------------------------------------------
@@ -53,13 +59,19 @@ var SonarbugServerApplication = Class.extend(Obj, {
          * @private
          * @type {IocContext}
          */
-        this.iocContext = new IocContext();
+        this.iocContext         = new IocContext();
 
         /**
          * @private
          * @type {ConfigurationScan}
          */
-        this.configurationScan = new ConfigurationScan(this.iocContext);
+        this.configurationScan  = new ConfigurationScan(new ConfigurationAnnotationProcessor(this.iocContext));
+
+        /**
+         * @private
+         * @type {ModuleScan}
+         */
+        this.moduleScan         = new ModuleScan(new ModuleAnnotationProcessor(this.iocContext));
     },
 
 
@@ -71,7 +83,8 @@ var SonarbugServerApplication = Class.extend(Obj, {
      * @param {function(Error)} callback
      */
     start: function(callback) {
-        this.configurationScan.scan();
+        this.configurationScan.scanAll();
+        this.moduleScan.scanAll();
         this.iocContext.process();
         this.iocContext.initialize(callback);
     }
