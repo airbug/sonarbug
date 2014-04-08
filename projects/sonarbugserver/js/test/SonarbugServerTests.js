3502f677c2d2c88b46e36de700c1768702aad02a
Brian Neisler <neisler.brian@gmail.com>
2014-04-08 11:25:10 -0700

    Update TestAnnotation package

--- projects/sonarbugserver/js/test/SonarbugServerTests.js
+++ projects/sonarbugserver/js/test/SonarbugServerTests.js
@@ -7,7 +7,7 @@
 //@Require('Class')
 //@Require('Obj')
 //@Require('bugmeta.BugMeta')
-//@Require('bugunit-annotate.TestAnnotation')
+//@Require('bugunit.TestAnnotation')
 //@Require('sonarbugserver.SonarbugServer')
 
 
@@ -25,7 +25,7 @@ var bugpack = require('bugpack').context();
 var Class               = bugpack.require('Class');
 var Obj                 = bugpack.require('Obj');
 var BugMeta             = bugpack.require('bugmeta.BugMeta');
-var TestAnnotation      = bugpack.require('bugunit-annotate.TestAnnotation');
+var TestAnnotation      = bugpack.require('bugunit.TestAnnotation');
 var SonarbugServer      = bugpack.require('sonarbugserver.SonarbugServer');
 
 
